852cff039166470352797e5afbd1402acd86e6e5
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-09-25 19:13:00 +0500

    Button in Process

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,48 +1,52 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import { useStateValue } from '../States/GlobalState'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import { useStateValue } from "../States/GlobalState";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Button from "@material-ui/core/Button";
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexWrap: 'wrap',
-      width: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      
-      Paper: {
-          width: '300',
-          height: '500',
-          
-      },
-    },
-  }));
-  
-
+  root: {
+    flexWrap: "wrap",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Paper: {
+    width: "100%",
+    height: "150px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightblue",
+    flexFlow: "column",
+  },
+}));
 
 const Checkout = () => {
-    const [{basket}] = useStateValue();
-    const classes = useStyles();
-    
-   return(
-    <div className={classes.root}>
-       {basket.length === 0 ? 
-        <div className={classes.emptyBasket}>
-        
-        <Paper className={classes.Paper} elevation={3}>
-            The Cart is Empty
-        </Paper>    
-        
-        </div>:
-        <div className={classes.nonEmptyBasket}>
-            
-            Basket is not Empty
-        
-        </div> }
-       </div>
-   );
-}
+  const classes = useStyles();
+  const [{ basket }] = useStateValue();
 
-export default Checkout
+  return (
+    <div className={classes.root}>
+      {basket.length === 0 ? (
+        <Paper className={classes.Paper} elevation={3}>
+          <h2 className={classes.text}>Your Cart is Empty</h2>
+          <Button
+            size="small"
+            className={classes.button}
+            
+          >
+            <AddShoppingCartIcon /> Add to Cart
+          </Button>
+        </Paper>
+      ) : (
+        <div className={classes.nonEmptyBasket}>Basket is not Empty</div>
+      )}
+    </div>
+  );
+};
+
+export default Checkout;
